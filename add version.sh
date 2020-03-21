a166b0682a40cc31447d9cd2cd978aec9e8e8ca7
silicondev <47757443+silicondev@users.noreply.github.com>
2020-03-21 21:57:08 +0000

    Removed version folders.
    They did not work in other projects,

--- add version.sh
+++ add version.sh
@@ -15,9 +15,7 @@ read VERSION
 
 pause "Creating a new version as $GROUP_ID.$ARTIFACT_ID v$VERSION. Press enter to continue."
 
-mkdir $VERSION
-mv $JAR $VERSION
-cd $VERSION
+rm -rf io
 mvn install:install-file -DgroupId=$GROUP_ID -DartifactId=$ARTIFACT_ID -Dversion=$VERSION -Dfile=$JAR -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
 rm $JAR
 pause "Extraction complete. Version $VERSION created. Press enter to close."

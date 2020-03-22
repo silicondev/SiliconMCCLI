65c1bb05f6286e6d661c16c34631597934bccbb8
silicondev <47757443+silicondev@users.noreply.github.com>
2020-03-22 01:18:08 +0000

    v0.0.3
    Added debug mode to command handler.

--- src/main/java/io/github/silicondev/siliconmccli/Handler.java
+++ src/main/java/io/github/silicondev/siliconmccli/Handler.java
@@ -10,13 +10,29 @@ import org.bukkit.command.CommandSender;
 public class Handler implements CommandExecutor {
 	
 	private List<CLICommand> _commands;
+	private boolean _debug = false;
 	
-	public Handler(List<CLICommand> listedCommands) {
-		_commands = listedCommands;
+	public Handler(List<CLICommand> commands) {
+		_commands = commands;
+	}
+	
+	public Handler(List<CLICommand> commands, boolean debug) {
+		_commands = commands;
+		_debug = debug;
 	}
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		
+		if (_debug) {
+			
+			String argString = "";
+			for (String str : args) {
+				argString += str + " ";
+			}
+			
+			sender.sendMessage("[SiliconMCCLI] Received command input: " + command.getName() + argString.trim());
+		}
 		
 		for (CLICommand cmd : _commands) {
 			if (cmd.Input.equalsIgnoreCase(command.getName())) {
